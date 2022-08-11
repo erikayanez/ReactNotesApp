fdaa1b79eb047de389959c84f8ddf8aa75f5ccf2
“erikayanez“ <erikajoam@gmail.com>
2022-08-11 22:58:07 +0200

    New components created

--- Project/notes-app/src/App.js
+++ Project/notes-app/src/App.js
@@ -1,10 +1,34 @@
+import { useState } from 'react'
+import { nanoid} from 'nanoid'
 import NotesList from './Components/NotesList';
 import './App.css';
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note!",
+      date: "10/08/2022"
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note!",
+      date: "10/08/2022"
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note!",
+      date: "10/08/2022"
+    },
+    {
+      id: nanoid(),
+      text: "This is my new note!",
+      date: "10/08/2022"
+    },
+  ])
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes={notes}/>
     </div>
   );
 }
